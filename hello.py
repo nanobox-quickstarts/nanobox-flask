d85e53f6feadfc3e54f04267881ff5dd418b1dde
Steve Domino <domino.steve@gmail.com>
2016-09-19 19:54:42 -0600

    updated comment

--- hello.py
+++ hello.py
@@ -6,4 +6,7 @@ def hello():
     return "Hello nanobox!"
 
 if __name__ == "__main__":
+
+    # nanobox configuration; most apps bind to localhost by default, however we
+    # need to allow connections from the host into the container
     app.run(host='0.0.0.0', port=8080)
